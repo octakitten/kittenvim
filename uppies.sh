27331fb65139dc242cf565f60a76f0eb2e7b8a89
octakitten <raspberry2425@gmail.com>
2024-10-18 20:07:05 -0400

    fixing path

--- uppies.sh
+++ uppies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$KVIM_START_DIR="$1"
+$KVIM_START_DIR="$KITTENVIM_DIR"
 $XDG_CONFIG_HOME="$XDG_CONFIG_HOME:$KVIM_START_DIR/.config"
 $XDG_DATA_HOME="$XDG_DATA_HOME:$KVIM_START_DIR/.local/share"
 $XDG_STATE_HOME="$XDG_STATE_HOME:$KVIM_START_DIR/.local/state"
@@ -9,5 +9,5 @@ $XDG_CACHE_HOME="$XDG_CACHE_HOME:$KVIM_START_DIR/.cache/nvim"
 $NVIM_LOG_FILE="$NVIM_LOG_FILE:$KVIM_START_DIR/.local/state/nvim"
 #$XDG_CONFIG_DIRS="$XDG_CONFIG_DIRS:$KVIM_START_DIR/etc/xdg"
 #$XDG_DATA_DIRS="$XDG_DATA_DIRS:$KVIM_START_DIR/usr/local/share:$KVIM_START_DIR/usr/share"
-$NVIM_APPNAME='kittenvim'
-alias kvim='nvim' 
+$NVIM_APPNAME="kittenvim"
+alias kvim="nvim" 
