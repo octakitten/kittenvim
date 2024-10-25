527291872b81dcfa797afb385dbe672145f37af0
octakitten <raspberry2425@gmail.com>
2024-10-25 16:05:38 -0400

    fixing keybinds

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -2,6 +2,6 @@ local wk = require("which-key")
 wk.add({
   { ";", ":", desc = "CMD enter command mode", mode = "n"},
   { "jk", "<ESC>", mode = "i" , desc = "Escape" },
-  { "wq", "<cmd> wq", mode = "n", desc = "write/quit" },
+  { "wq", "ZZ", mode = "n", desc = "write/quit" },
   { "qq", "ZQ", mode = "n", desc = "quit/all" },
 })
